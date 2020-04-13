28865fd2d001756162b870a05c06ec9ada7d407c
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-04-13 12:19:58 -0400

    Update with firewall

--- datadog/setup/main.tf
+++ datadog/setup/main.tf
@@ -9,6 +9,20 @@ provider "google" {
   zone    = "us-central1-a"
 }
 
+data "google_compute_network" "default" {
+  name = "default"
+}
+
+resource "google_compute_firewall" "ecommerce" {
+  name    = "allow-ecommerce"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3000"]
+  }
+}
+
 resource "google_compute_instance" "default" {
   name         = "datadog-webinar-ecommerce"
   machine_type = "n1-standard-1"
