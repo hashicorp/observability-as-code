e3d445ccfd4463fec725d0c7f88e5156a342f2e3
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-04-20 11:55:56 -0400

    Update monitors

--- datadog/monitors.tf
+++ datadog/monitors.tf
@@ -29,7 +29,7 @@ resource "datadog_monitor" "apm_service_high_avg_latency" {
   message            = "Service ${each.key} has a high average latency. @pagerduty-${each.key}"
   escalation_message = "Service ${each.key} has a high average latency!! @pagerduty-${each.key}"
 
-  query = "avg(last_10m): ( sum:trace.rack.request.duration{service:${each.key},env:${var.environment}}.rollup(sum).fill(zero) / sum:trace.rack.request.hits{service:${each.key},env:${var.environment}}.rollup(sum).fill(zero) )"
+  query = "avg(last_10m): ( sum:trace.rack.request.duration{service:${each.key},env:${var.environment}}.rollup(sum).fill(zero)/sum:trace.rack.request.hits{service:${each.key},env:${var.environment}}.rollup(sum).fill(zero) ) > ${each.value.high_avg_latency_critical}"
 
   thresholds = {
     warning  = each.value.high_avg_latency_warning
@@ -53,7 +53,7 @@ resource "datadog_monitor" "apm_service_high_p90_latency" {
   message            = "Service ${each.key} has a high p90 latency. @pagerduty-${each.key}"
   escalation_message = "Service ${each.key} has a high p90 latency!! @pagerduty-${each.key}"
 
-  query = "avg(last_10m):trace.rack.request.duration.by.service.90p{service:${each.key},env:${var.environment}} > 1"
+  query = "avg(last_10m):trace.rack.request.duration.by.service.90p{service:${each.key},env:${var.environment}} > ${each.value.high_p90_latency_critical}"
 
   thresholds = {
     warning  = each.value.high_p90_latency_warning
