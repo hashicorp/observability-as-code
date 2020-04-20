604defff62ee36c050570cb6a946f22f907b3627
Daniel Maher <daniel.maher@datadoghq.com>
2020-04-20 15:24:54 +0200

    add "tuned" latency monitors

--- datadog/monitors.tf
+++ datadog/monitors.tf
@@ -1,15 +1,15 @@
-resource "datadog_monitor" "apm" {
+resource "datadog_monitor" "apm_service_high_error_rate" {
   for_each           = var.services
-  name               = "Service ${each.key} has a high error rate"
+  name               = "Service ${each.key} has a high error rate on ${var.environment}"
   type               = "query alert"
   message            = "Service ${each.key} has a high error rate. @pagerduty-${each.key}"
-  escalation_message = "Service ${each.key} has a high error rate. @pagerduty-${each.key}"
+  escalation_message = "Service ${each.key} has a high error rate!! @pagerduty-${each.key}"
 
   query = "avg(last_10m):sum:trace.rack.request.errors{env:${var.environment},service:${each.key} } / sum:trace.rack.request.hits{env:${var.environment},service:${each.key} } > ${each.value.critical}"
 
   thresholds = {
-    warning  = each.value.warning
-    critical = each.value.critical
+    warning  = each.value.high_error_rate_warning
+    critical = each.value.high_error_rate_critical
   }
 
   notify_no_data    = false
@@ -21,3 +21,52 @@ resource "datadog_monitor" "apm" {
 
   tags = ["service:${each.key}", "env:${var.environment}"]
 }
+
+resource "datadog_monitor" "apm_service_high_avg_latency" {
+  for_each           = var.services
+  name               = "Service ${each.key} has a high average latency on ${var.environment}"
+  type               = "query alert"
+  message            = "Service ${each.key} has a high average latency. @pagerduty-${each.key}"
+  escalation_message = "Service ${each.key} has a high average latency!! @pagerduty-${each.key}"
+
+  query = "( sum:trace.rack.request.duration{service:${each.key},env:${var.environment}}.rollup(sum).fill(zero) / sum:trace.rack.request.hits{service:${each.key},env:${var.environment}}.rollup(sum).fill(zero) )"
+
+  thresholds = {
+    warning  = each.value.high_avg_latency_warning
+    critical = each.value.high_avg_latency_critical
+  }
+
+  notify_no_data    = false
+  renotify_interval = 0
+
+  notify_audit = false
+  timeout_h    = 0
+  include_tags = true
+
+  tags = ["service:${each.key}", "env:${var.environment}"]
+}
+
+resource "datadog_monitor" "apm_service_high_p90_latency" {
+  for_each           = var.services
+  name               = "Service ${each.key} has a high p90 latency on ${var.environment}"
+  type               = "query alert"
+  message            = "Service ${each.key} has a high p90 latency. @pagerduty-${each.key}"
+  escalation_message = "Service ${each.key} has a high p90 latency!! @pagerduty-${each.key}"
+
+  query = "avg:trace.rack.request.duration.by.service.90p{service:${each.key},env:${env.environment}}"
+
+  thresholds = {
+    warning  = each.value.high_p90_latency_warning
+    critical = each.value.high_p90_latency_critical
+  }
+
+  notify_no_data    = false
+  renotify_interval = 0
+
+  notify_audit = false
+  timeout_h    = 0
+  include_tags = true
+
+  tags = ["service:${each.key}", "env:${var.environment}"]
+}
+
--- datadog/terraform.auto.tfvars
+++ datadog/terraform.auto.tfvars
@@ -3,7 +3,11 @@ environment = "ruby-shop"
 services = {
   store-frontend = {
     pd_service_key = "54321098765432109876"
-    critical       = 10
-    warning        = 8
+    high_error_rate_critical      = 10
+    high_error_rate_warning       =  8
+    high_avg_latency_critical     =  3
+    high_avg_latency_warning      =  1
+    high_p90_latency_critical     =  6
+    high_p90_latency_warning      =  4
   }
 }
