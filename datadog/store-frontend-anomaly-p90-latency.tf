4af1d1602020aff58b4e3cccc43e247942edf99b
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-04-23 10:50:11 -0400

    Add critical threshold to anomaly

--- datadog/store-frontend-anomaly-p90-latency.tf
+++ datadog/store-frontend-anomaly-p90-latency.tf
@@ -1,15 +1,21 @@
+locals {
+  critical          = 0.75
+  warning           = 0.66
+  critical_recovery = 0
+}
+
 resource "datadog_monitor" "apm_store_frontend_anomalous_p90_latency" {
   name               = "Service store-frontend has an anomalous p90 latency on ruby-shop"
   type               = "query alert"
   message            = "Service store-fronted has an anomalous p90 latency on ruby-shop."
   escalation_message = "Service store-fronted has an anomalous p90 latency on ruby-shop!!"
 
-  query = "avg(last_1h):anomalies(avg:trace.rack.request.duration.by.service.90p{service:store-frontend,env:ruby-shop}, 'basic', 2, direction='above', interval=20)"
+  query = "avg(last_1h):anomalies(avg:trace.rack.request.duration.by.service.90p{service:store-frontend,env:ruby-shop}, 'basic', 2, direction='above', interval=20) >= ${local.critical}"
 
   thresholds = {
-    critical          = 0.75
-    warning           = 0.66
-    critical_recovery = 0
+    critical          = local.critical
+    warning           = local.warning
+    critical_recovery = local.critical_recovery
   }
 
   threshold_windows = {
